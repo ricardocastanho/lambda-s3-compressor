e705dc464fbd648b5c5e69f3ec488ff54009b0ba
ricardocastanho <48383095+ricardocastanho@users.noreply.github.com>
2023-10-10 11:30:30 -0300

    test: update tests

--- test.js
+++ test.js
@@ -1,6 +1,21 @@
-import { handler } from "./index.js"
+import crypto from "crypto";
+import { handler } from "./index.js";
 
 (async () => {
-  const response = await handler({ bucket: "compressor-dev", key: "test.png" });
+  const response = await handler({
+    invocationSchemaVersion: "1.0",
+    invocationId: crypto.randomUUID(),
+    job: {
+      id: crypto.randomUUID(),
+    },
+    tasks: [
+      {
+        taskId: crypto.randomUUID(),
+        s3Key: "test.png",
+        s3VersionId: "1",
+        s3BucketArn: "arn:aws:s3:::compressor-dev",
+      },
+    ],
+  });
   console.log({ response });
 })();
